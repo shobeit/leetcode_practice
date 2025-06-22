17b336758d08887035f140b3727b3e9819c3700d
Justin T <justinkeola84@gmail.com>
2025-06-22 14:44:46 -0700

    containsDupeHashset

---
+++ ContainsDuplicate.py
@@ -0,0 +1,12 @@
+#Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+
+        my_set = set()
+
+        for n in nums:
+            if n in my_set:
+                return True
+            my_set.add(n)
+        return False
