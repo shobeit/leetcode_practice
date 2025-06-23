33d93b052352c9e51e3470eca1b2c23a781232c4
Justin T <justinkeola84@gmail.com>
2025-06-22 19:27:50 -0700

    maxSubArray

---
+++ MaxSubarray.py
@@ -0,0 +1,14 @@
+# Given an array of integers nums, find the subarray with the largest sum and return the sum.
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+
+        maxSub = nums[0]
+        curSum = 0
+
+        for n in nums:
+            if curSum < 0:
+                curSum = 0
+            curSum += n
+            maxSub = max(maxSub, curSum)
+        return maxSub
