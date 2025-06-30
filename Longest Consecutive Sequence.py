d44824e2aee76b75464fbcf233a0bc2942e4567f
Justin T <justinkeola84@gmail.com>
2025-06-29 23:46:19 -0700

    LCseq

---
+++ Longest Consecutive Sequence.py
@@ -0,0 +1,21 @@
+# Longest Consecutive Sequence
+
+# Given an array of integers nums, return the length of the longest consecutive sequence of elements that can be formed.
+
+# A consecutive sequence is a sequence of elements in which each element is exactly 1 greater than the previous element. The elements do not have to be consecutive in the original array.
+
+# You must write an algorithm that runs in O(n) time.
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+
+        numSet = set(nums)
+        longest = 0
+
+        for n in nums:
+            if (n - 1) not in numSet:
+                length = 0
+                while (n + length) in numSet:
+                    length += 1
+                longest = max(length, longest)
+        return longest
