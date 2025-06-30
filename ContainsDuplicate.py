2804f09e681a1012b73934fafc9ffc24af673d3f
Justin T <justinkeola84@gmail.com>
2025-06-29 20:15:30 -0700

    practice

--- ContainsDuplicate.py
+++ ContainsDuplicate.py
@@ -1,4 +1,4 @@
-#Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
 
 class Solution:
     def hasDuplicate(self, nums: List[int]) -> bool:
