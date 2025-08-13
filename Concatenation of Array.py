24f1b275f81edd07ebaa378dc61557746658bd20
shobeit <justinkeola84@gmail.com>
2025-08-12 21:40:08 -0700

    practice

--- Concatenation of Array.py
+++ Concatenation of Array.py
@@ -2,6 +2,7 @@
 #You are given an integer array nums of length n. Create an array ans of length 2n where ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
 #Specifically, ans is the concatenation of two nums arrays.
 #6/21/2025
+#8/12/2025
 
 
 
@@ -20,4 +21,5 @@ class Solution:
 sol = Solution()
 
 print(sol.getConcatenation([1, 4, 1, 2]))
+print(sol.getConcatenation([22,21,20,1]))
 print(sol.getConcatenation([22,21,20,1]))
